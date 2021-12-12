225ea183529cbd0d1dad31d4bafe37472c4f9678
Portico Exchange <95722953+PorticoExchange@users.noreply.github.com>
2021-12-12 16:33:03 -0300

    Create SwapLiquid.js

---
+++ contracts/SwapLiquid.js
@@ -0,0 +1,8 @@
+{
+  "version": 0,
+  "issuer_pubkey": "037c7db0528e8b7b58e698ac104764f6852d74b5a7335bffcdad0ce799dd7742ec",
+  "name": "Foo Coin",
+  "ticker": "FOO",
+  "entity": { "domain": "foo-coin.com" },
+  "precision":8
+}
